f47b7e26ea7dcf2d4956a5d492f8245ec0a02ed2
Michael Sankovich <misankovich@gmail.com>
2016-08-27 18:57:20 -0400

    initial

---
+++ index.js
@@ -0,0 +1,23 @@
+// App Starting Point
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./routes/index');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:auth/auth');
+
+//APP SETUP
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*'}));
+
+router(app);
+
+
+//SERVER SETUP
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on Port: ', port);
